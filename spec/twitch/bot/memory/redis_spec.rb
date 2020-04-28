9c4c46d5cbed0b4c768925e0cff95a1bbe37c173
Jochen Lillich <contact@geewiz.dev>
2020-04-28 21:33:14 +0100

    Replace REDIS_HOST/REDIS_PORT with REDIS_URL
    This is the way.

--- spec/twitch/bot/memory/redis_spec.rb
+++ spec/twitch/bot/memory/redis_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Twitch::Bot::Memory::Redis do
   describe "#store" do
-    it "persists a value for a key" do
+    it "works with default connection details" do
       config = Twitch::Bot::Config.new(
         settings: {
           bot_user: "testuser",
@@ -18,5 +18,44 @@ RSpec.describe Twitch::Bot::Memory::Redis do
 
       expect(mem.retrieve("foo")).to eq "bar"
     end
+  end
+
+  it "works with ENV connection details" do
+    ENV["REDIS_URL"] = "redis://localhost:6379"
+    config = Twitch::Bot::Config.new(
+      settings: {
+        bot_user: "testuser",
+      },
+    )
+    client = Twitch::Bot::Client.new(
+      config: config,
+      channel: "testchannel",
+    )
+    mem = described_class.new(client: client)
+
+    mem.store("foo", "bar")
+
+    expect(mem.retrieve("foo")).to eq "bar"
+  end
+
+  it "works with config connection details" do
+    config = Twitch::Bot::Config.new(
+      settings: {
+        bot_user: "testuser",
+        redis: {
+          host: "localhost",
+          port: 6379,
+        },
+      },
+    )
+    client = Twitch::Bot::Client.new(
+      config: config,
+      channel: "testchannel",
+    )
+    mem = described_class.new(client: client)
+
+    mem.store("foo", "bar")
+
+    expect(mem.retrieve("foo")).to eq "bar"
   end
 end
