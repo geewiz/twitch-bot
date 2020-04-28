68126b7aca2848bb5f1d07f37df4a4a8534bf2af
Jochen Lillich <contact@geewiz.dev>
2020-04-28 21:55:04 +0100

    Fix Redis connection tests
    We can't test the hard-wired default values.

--- spec/twitch/bot/memory/redis_spec.rb
+++ spec/twitch/bot/memory/redis_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Twitch::Bot::Memory::Redis do
   describe "#store" do
-    it "works with default connection details" do
+    it "works with ENV connection details" do
       config = Twitch::Bot::Config.new(
         settings: {
           bot_user: "testuser",
@@ -18,44 +18,31 @@ RSpec.describe Twitch::Bot::Memory::Redis do
 
       expect(mem.retrieve("foo")).to eq "bar"
     end
-  end
 
-  it "works with ENV connection details" do
-    ENV["REDIS_URL"] = "redis://localhost:6379"
-    config = Twitch::Bot::Config.new(
-      settings: {
-        bot_user: "testuser",
-      },
-    )
-    client = Twitch::Bot::Client.new(
-      config: config,
-      channel: "testchannel",
-    )
-    mem = described_class.new(client: client)
+    it "works with config connection details" do
+      # Override environment variable
+      url = URI.parse(ENV["REDIS_URL"])
+      ENV["REDIS_URL"] = nil
 
-    mem.store("foo", "bar")
-
-    expect(mem.retrieve("foo")).to eq "bar"
-  end
-
-  it "works with config connection details" do
-    config = Twitch::Bot::Config.new(
-      settings: {
-        bot_user: "testuser",
-        redis: {
-          host: "localhost",
-          port: 6379,
+      config = Twitch::Bot::Config.new(
+        settings: {
+          bot_user: "testuser",
+          redis: {
+            host: url.host,
+            port: url.port,
+          },
         },
-      },
-    )
-    client = Twitch::Bot::Client.new(
-      config: config,
-      channel: "testchannel",
-    )
-    mem = described_class.new(client: client)
+      )
+      client = Twitch::Bot::Client.new(
+        config: config,
+        channel: "testchannel",
+      )
+      mem = described_class.new(client: client)
 
-    mem.store("foo", "bar")
+      mem.store("foo", "bar")
 
-    expect(mem.retrieve("foo")).to eq "bar"
+      expect(mem.retrieve("foo")).to eq "bar"
+      ENV["REDIS_URL"] = url.to_s
+    end
   end
 end
